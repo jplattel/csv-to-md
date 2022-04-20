89a3d1b8df9bc2f8838b87f069c4a6bddf488bbb
Joost Plattel <jsplattel@gmail.com>
2022-04-20 17:23:33 +0200

    only preview the first 100

--- src/Preview.svelte
+++ src/Preview.svelte
@@ -1,5 +1,6 @@
 <div>
-    {#each csv.data as data}
+    <p>Only the first 100 are previewed to preserve performance...</p>
+    {#each csv.data.slice(0,100) as data}
         <div class="card mb-3">
             <div class="card-body">
                 <h5 class="card-title">{render(templateFilename, data)}.md</h5>
